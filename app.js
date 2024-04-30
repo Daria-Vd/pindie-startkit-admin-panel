8eae331c47988876fca9b9f5e7f15baf8e6e5c8f
Dariya Vedernikova <daria-vedernikova08@yandex.ru>
2024-04-30 20:20:30 +0300

    init: start project

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mainRoute = require("./routes/main");
+const gamesRouter = require("./routes/games");
+const { cors } = require("./middlewares/cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, "public")),
+    mainRoute,
+    gamesRouter
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT: http://localhost:${PORT}`);
+});
---
+++ utils/data/parse.js
@@ -0,0 +1,17 @@
+const fs = require("fs").promises;
+
+const readData = async (path) => {
+    try {
+        const data = await fs.readFile(path, "utf-8");
+        return JSON.parse(data);
+    } catch (error) {
+        console.log("error: ", error);
+    }
+};
+
+const writeData = async (path, data) => {
+    const json = JSON.stringify(data);
+    await fs.writeFile(path, json);
+};
+
+module.exports = { readData, writeData };
